dd0e922d83bcd68add78a9e5c1a0476f027783fd
akashkumar55 <akvakv150@gmail.com>
2025-07-01 23:19:45 +0530

    Created Home Page

---
+++ lib/home/widget/drawer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'home_appbar.dart';
+
+
+class drawer extends StatelessWidget {
+  const drawer({
+    super.key,
+    required this.icons,
+    required this.titles,
+  });
+
+  final List<IconData> icons;
+  final List<String> titles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurple.shade900,
+              Colors.blueAccent.shade100
+            ]
+          )
+        ),
+        padding: EdgeInsets.only(top: 80),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage("images/pic.jpg"),
+            ),
+            Text("Akash Kumar Verma",style: TextStyle(color: Colors.white,fontSize: 20)),
+            Text("akashverma@gmail.com",style: TextStyle(color: Colors.white,fontSize: 14)),
+            divider(),
+            Container(
+              width: double.infinity,
+              height: 400,
+              padding: EdgeInsets.only(top: 10),
+              child: ListView.builder(
+                itemCount: icons.length,
+                itemBuilder: (context,index){
+                  return InkWell(
+                    onTap: (){
+                      print(titles[index]+' Item Pressed!');
+                    },
+                    child: ListTile(
+                      leading: Icon(icons[index],color: Colors.lightBlueAccent,size: 30,),
+                      title: Text(titles[index],style: TextStyle(color: Colors.white,fontSize: 18),),
+                    ),
+                  );
+                },
+              ),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
---
+++ lib/home/widget/fab.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class floatingActionButton extends StatelessWidget {
+  const floatingActionButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("FAB Pressed");
+      },
+      child: Material(
+        elevation: 20,
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.blueGrey,
+          ),
+          child: Center(child: Icon(Icons.add, color: Colors.white)),
+        ),
+      ),
+    );
+  }
+}
